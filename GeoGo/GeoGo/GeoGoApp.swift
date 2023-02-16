3076ca375b925d09611036346eb6bd4f65fb16bd
johnstennett24 <93450112+johnstennett24@users.noreply.github.com>
2023-02-16 10:34:52 -0700

    added project

---
+++ GeoGo/GeoGo/GeoGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoGoApp.swift
+//  GeoGo
+//
+//  Created by John Stennett on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct GeoGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
